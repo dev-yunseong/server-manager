cfbba97cfa6b87a4c655cb7ae7dcdc6e6e216285
Yunseong <dev.yunseong@gmail.com>
2026-02-12 14:55:21 +0900

    feat: Implement ClientConfig, ServerConfig, and EventConfig adapters for configuration management

---
+++ src/infrastructure/config/client.rs
@@ -0,0 +1,32 @@
+use std::error::Error;
+use async_trait::async_trait;
+use crate::application::config::ClientConfigUseCase;
+use crate::domain::config::{ClientConfig, Config};
+use crate::infrastructure::common::file_accessor::{get_config_file_accessor, FileAccessor};
+
+pub struct ClientConfigAdapter {
+    config_file_accessor: FileAccessor<Config>
+}
+
+impl ClientConfigAdapter {
+    pub fn new() -> Self {
+        Self {
+            config_file_accessor: get_config_file_accessor()
+        }
+    }
+}
+
+#[async_trait]
+impl ClientConfigUseCase for ClientConfigAdapter {
+    async fn add_client(&self, client_config: ClientConfig) -> Result<(), Box<dyn Error>> {
+        let mut config = self.config_file_accessor.read().await?;
+        config.clients.push(client_config);
+        self.config_file_accessor.write(config).await?;
+        Ok(())
+    }
+
+    async fn list_client(&self) -> Result<Vec<ClientConfig>, Box<dyn Error>> {
+        let config = self.config_file_accessor.read().await?;
+        Ok(config.clients)
+    }
+}
---
+++ src/infrastructure/config/server.rs
@@ -0,0 +1,33 @@
+use std::error::Error;
+use async_trait::async_trait;
+use crate::application::config::ServerConfigUseCase;
+use crate::domain::config::{Config, ServerConfig};
+use crate::infrastructure::common::file_accessor::{get_config_file_accessor, FileAccessor};
+
+pub struct ServerConfigAdapter {
+    config_file_accessor: FileAccessor<Config>
+}
+
+impl ServerConfigAdapter {
+    pub fn new() -> Self {
+        Self {
+            config_file_accessor: get_config_file_accessor()
+        }
+    }
+}
+
+#[async_trait]
+impl ServerConfigUseCase for ServerConfigAdapter {
+
+    async fn add_server(&self, server_config: ServerConfig) -> Result<(), Box<dyn Error>> {
+        let mut config = self.config_file_accessor.read().await?;
+        config.servers.push(server_config);
+        self.config_file_accessor.write(config).await?;
+        Ok(())
+    }
+
+    async fn list_server(&self) -> Result<Vec<ServerConfig>, Box<dyn Error>> {
+        let config = self.config_file_accessor.read().await?;
+        Ok(config.servers)
+    }
+}
