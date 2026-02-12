95c72c73d43d38bc5fd451fffc7baff36091ae3d
Yunseong <dev.yunseong@gmail.com>
2026-02-12 13:55:48 +0900

    feat: Introduce ClientConfig and ServerConfig structures for improved configuration management

---
+++ src/domain/config/client.rs
@@ -0,0 +1,18 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct ClientConfig {
+    pub name: String,
+    pub kind: String, // ex: telegram
+    pub token: Option<String>
+}
+
+impl ClientConfig {
+    pub fn new_telegram(name: &str, token: &str) -> Self {
+        Self {
+            name: String::from(name),
+            kind: String::from("telegram"),
+            token: Some(String::from(token))
+        }
+    }
+}
---
+++ src/domain/config/server.rs
@@ -0,0 +1,24 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ServerConfig {
+    pub name: String,
+    pub base_url: Option<String>,
+    pub docker_container_name: Option<String>,
+    pub health_check_path: Option<String>,
+    pub kill_path: Option<String>,
+    pub log_command: Option<String>,
+}
+
+impl ServerConfig {
+    pub fn new(name: String, base_url: Option<String>, docker_container_name: Option<String>, health_check_path: Option<String>, kill_path: Option<String>, log_command: Option<String>,) -> Self {
+        Self {
+            name: String::from(name),
+            base_url,
+            docker_container_name,
+            health_check_path,
+            kill_path,
+            log_command
+        }
+    }
+}
